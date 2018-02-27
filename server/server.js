71010a63866c3c132bf0a0ddea4257ea5cdfd6e0
sgwanna <[sg0314@casgis.com]>
2018-02-27 11:54:46 +0900

    server 테스트

---
+++ server/server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+  response.writeHead(200, {'Content-Type':'text/html'});
+  response.end('Hello World!');
+});
+
+server.listen(8080, function() {
+  console.log('Server is running...');
+});
