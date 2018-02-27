1e48c7768d3fa8a4c3c8fed2352256851b09165d
sgwanna <[sg0314@casgis.com]>
2018-02-27 18:42:37 +0900

    mysql commit

--- mysql_test/index.js
+++ mysql_test/index.js
@@ -1,21 +1,58 @@
-var express = require('express');
+var express = require('./node_modules/express');
 var app = express();
-var mysql = require('mysql');
-var conn = mysql.createConnection({
-  host      : '172.16.25.102',
-  user      : 'root',
-  password  : 'castis',
-  port      : 3306,
-  database  : 'ADCAST_test'
+var mysql = require('./node_modules/mysql');
+var config = {
+  host            : '172.16.25.102',
+  user            : 'root',
+  password        : 'castis',
+  port            : 3306,
+  database        : 'ADCAST_test',
+  connectionLimit : 50
+};
+var pool = mysql.createPool(config);
+
+pool.getConnection(function(err, connection) {
+  if(!err) {
+    // 전체 청약 조회
+    app.get('/getADContractList', function(req, res) {
+      var queryStr = 'SELECT ID, TITLE, EXECUTION_START_DATE, EXECUTION_END_DATE FROM TBL_AD_CONTRACT';
+
+      connection.query(queryStr, function(err, rows, fields) {
+        if(!err) {
+          var result = {
+            size: rows.length,
+            result: rows
+          };
+          res.json(result);
+          console.log(result);
+        } else {
+          console.log('Error[%s]', err);
+        }
+      });
+    });
+
+    // title로 청약 조회
+    app.get('/getADContract', function(req, res) {
+      var queryStr = 'SELECT ID, TITLE, EXECUTION_START_DATE, EXECUTION_END_DATE FROM TBL_AD_CONTRACT ';
+      queryStr += 'WHERE TITLE LIKE ("%'+req.query.title+'%")';
+      for (var key in req.query) {
+        console.log(key);
+      }
+      connection.query(queryStr, function(err, rows, fields) {
+        if(!err) {
+          var result = {
+            size: rows.length,
+            result: rows
+          };
+          res.json(result);
+          console.log(result);
+        } else {
+          console.log('Error[%s]', err);
+        }
+      });
+    });
+  }
+  connection.release();
 });
 
-conn.connect();
-
-conn.query('SELECT * FROM TBL_AD_CAMPAIGN_LOG ORDER BY ID DESC LIMIT 1', function(err, rows, fields) {
-  if(!err)
-    console.log('result: ' + rows);
-  else
-    console.log('Error[%s]', err);
-});
-
-conn.end();
+app.listen(3000);
